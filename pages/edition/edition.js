b872a9d9948a14c209aeb0ef95bb5a5745c9075c
chiweiwei2008 <chiweiwei2008@163.com>
2020-03-28 23:03:34 +0800

    解决版本说明无法显示问题

--- pages/edition/edition.js
+++ pages/edition/edition.js
@@ -5,8 +5,19 @@ Page({
 		article: {}
 	},
 	onLoad: function () {
+			 // 调用数据库，将数据写入数据库
+			 var that=this
+				wx.cloud.init()
+				const db = wx.cloud.database()
+				db.collection('edition').doc('1').get().then(res => {
+					//console.log(res.data)
+					that.setData({
+						article:res.data.article_code
+					})
+				})
+		//手动提取数据		
+		if (false){
 		const _ts = this;
-
 		app.getText('https://706d-pmis-4c63d-1300406220.tcb.qcloud.la/README.md',res => {
 			let obj = app.towxml(res.data,'markdown',{
 				// theme:'dark',
@@ -20,10 +31,26 @@ Page({
 				}
 			});
 
-			_ts.setData({
-				article:obj,
-			});
+			// _ts.setData({
+			// 	article:obj,
+			// });
+			// 调用数据库，将数据写入数据库
+				wx.cloud.init()
+				const db = wx.cloud.database({
+					env: 'pmis-4c63d'
+				})
+				db.collection('edition').doc('1').set({
+					// data 传入需要局部更新的数据
+					data: {
+						article_code: obj
+					}
+				})
+				.then(console.log)
+				.catch(console.error)
+			
+			console.log(obj)
 		});
+
 		
 	}
-})
+}})
